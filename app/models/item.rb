c3af59aa107eb77d2591e0290dcc4899849c7220
fujimasa0107 <158474627+fujimasa0107@users.noreply.github.com>
2024-02-23 16:36:27 +0900

    ActiveHashのバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,13 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :condition, :shipping_cost, :prefecture, :shipping_date
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_date
 
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :category_id, :condition_id, :shipping_cost_id, :prefecture_id, 
+            :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank" } 
+
 end
