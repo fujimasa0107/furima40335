b5fa8178e9307f2bdebb8fc47f7a0ca2d6047b71
fujimasa0107 <158474627+fujimasa0107@users.noreply.github.com>
2024-02-24 13:50:29 +0900

    出品した商品をトップページ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
