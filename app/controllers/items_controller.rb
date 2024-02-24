c9bf95b4af0427511268c909e63bfe4b7a97cb09
fujimasa0107 <158474627+fujimasa0107@users.noreply.github.com>
2024-02-24 15:17:47 +0900

    詳細ページの追加、ルーティング、コントローラー追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,9 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def show
+  end
+  
   def create
     @item = Item.new(item_params)
     if @item.save
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   
   root to: 'items#index'
   resources :users
-  resources :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create, :show,]
 end
