94f3e68b217c4a931a3c764fdf2443e0c4aa567f
fujimasa0107 <158474627+fujimasa0107@users.noreply.github.com>
2024-02-26 21:50:59 +0900

    ルーティング（ネスト）・ビュー追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   
   root to: 'items#index'
   resources :users
-  resources :items, only: [:index, :new, :create, :show,]
+  resources :items, only: [:index, :new, :create, :show,] do
+    resources :buyers, only: [:index, :create]
+  end
 end
