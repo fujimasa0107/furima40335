d9dc6e5c500697c00c25942a9da77f234d23406b
fujimasa0107 <158474627+fujimasa0107@users.noreply.github.com>
2024-02-26 21:53:18 +0900

    モデル、バリデーションなどを追加

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,9 @@
 class Destination < ApplicationRecord
   belongs_to :buyer
+
+  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
+  validates :prefecture_id, numericality: { other_than: 0 }
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
 end
